691bb0f34be44b2967fe338acbf3837ead56c92b
Grzegorz Miejski <grz.miejski@gmail.com>
2015-03-13 17:11:36 +0100

    Initial commit

---
+++ src/main/java/mobi/braincode/pushegro/PushegroServerApplication.java
@@ -0,0 +1,12 @@
+package mobi.braincode.pushegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushegroServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PushegroServerApplication.class, args);
+    }
+}
