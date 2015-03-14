f5ad3025f6fa605fd08de51b8ddcd12d6743ac76
Grzegorz Miejski <grz.miejski@gmail.com>
2015-03-14 15:01:31 +0100

    Server for improved notifications.

--- src/main/java/mobi/braincode/pushegro/gcm/GcmNotifier.java
+++ src/main/java/mobi/braincode/pushegro/gcm/GcmNotifier.java
@@ -1,8 +1,12 @@
 package mobi.braincode.pushegro.gcm;
 
 import mobi.braincode.pushegro.domain.User;
+import mobi.braincode.pushegro.domain.Watcher;
+import mobi.braincode.pushegro.domain.predicate.AuctionPredicate;
 import mobi.braincode.pushegro.gcm.api.Message;
 import mobi.braincode.pushegro.gcm.api.Sender;
+import mobi.braincode.pushegro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -15,12 +19,26 @@ public class GcmNotifier {
 
     public static final String API_CONSOLE_KEY = "AIzaSyCzqbXwPQm-A1xskxgHZc93z5GcN3rgDqE";
     public static final String SERVER_MESSAGE = "predicatesChanged";
+    public static final String SERVER_PREDICATES_NAMES = "predicatesNamesChanged";
+
+    @Autowired
+    private UserRepository userRepository;
 
     public void notify(User user, List<Long> predicatesChanged) {
         List<String> predicatesIdsInString = predicatesChanged.stream().map(String::valueOf).collect(toList());
 
+        User completeUser = userRepository.loadUserByUsername(user.getUsername());
+        List<String> auctionNames = completeUser.getWatchers().stream()
+                .map(Watcher::getPredicate)
+                .filter(x -> predicatesChanged.contains(x.getPredicateId()))
+                .map(AuctionPredicate::getKeyword)
+                .collect(toList());
+
         String joinedPredicatesIds = String.join(",", predicatesIdsInString);
+        String joinedAuctionNames = String.join(",", auctionNames);
+
         Message build = new Message.Builder().addData(SERVER_MESSAGE, joinedPredicatesIds)
+                .addData(SERVER_PREDICATES_NAMES, joinedAuctionNames)
                 .timeToLive(10)
                 .build();
         try {
--- src/main/java/mobi/braincode/pushegro/repository/InMemoryUserRepository.java
+++ src/main/java/mobi/braincode/pushegro/repository/InMemoryUserRepository.java
@@ -1,40 +1,15 @@
 package mobi.braincode.pushegro.repository;
 
 import mobi.braincode.pushegro.domain.User;
-import mobi.braincode.pushegro.domain.predicate.AuctionPredicate;
-import mobi.braincode.pushegro.scheduler.ScheduledWatcher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
 import java.util.HashSet;
 import java.util.Set;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
 
-    @Autowired
-    private ScheduledWatcher scheduledWatcher;
-
     private Set<User> users = new HashSet<>();
-
-    public InMemoryUserRepository() {
-    }
-
-    @PostConstruct
-    private void initWithDemoUsers() {
-        User demoUserOne = new User("luk", "gcmId");
-        AuctionPredicate auctionPredicateOne = new AuctionPredicate("dragon ball");
-        User demoUserTwo = new User("grzesiek", "grzesiekId");
-        AuctionPredicate auctionPredicateTwo = new AuctionPredicate("maczeta");
-
-        users.add(demoUserOne);
-        demoUserOne.addWatcher(auctionPredicateOne);
-        scheduledWatcher.registerUser(demoUserOne);
-        users.add(demoUserTwo);
-        demoUserTwo.addWatcher(auctionPredicateTwo);
-        scheduledWatcher.registerUser(demoUserTwo);
-    }
 
     @Override
     public void registerUser(User user) {
